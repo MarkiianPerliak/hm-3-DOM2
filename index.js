676e4f0084455b338eecaeba4395f9e4a17bc11d
MarkiianPerliak <122627880+MarkiianPerliak@users.noreply.github.com>
2025-02-10 17:32:52 +0200

    commit

--- index.js
+++ index.js
@@ -28,9 +28,62 @@ const ingredientsList = document.querySelector('#ingredients');
 const listItems = ingredients.map(ingredient => {
     const li = document.createElement('li');
     li.textContent = ingredient;
-    return li;
-});
+    return li.outerHTML;
+}).join('');
 
-ingredientsList.append(...listItems);
+ingredientsList.innerHTML = listItems;
 // 3
+const images = [
+    {
+      url: 'https://i.guim.co.uk/img/media/d059b58efe8ce50d15639f76448becdeec69bc9b/0_184_7200_4320/master/7200.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=cdb5298773baf69c080a77c757fd6c08',
+      alt: 'Programming',
+    },
+    {
+      url: 'https://cdn.builtin.com/cdn-cgi/image/f=auto,fit=cover,w=1200,h=635,q=80/https://builtin.com/sites/www.builtin.com/files/2024-09/programming-languages.jpg',
+      alt: 'Programming',
+    },
+    {
+      url: 'https://www.codingem.com/wp-content/uploads/2021/10/juanjo-jaramillo-mZnx9429i94-unsplash-1024x683.jpg',
+      alt: 'Programming',
+    },
+  ];
+  
+  const gallery = document.querySelector('#gallery');
 
+  gallery.insertAdjacentHTML('beforeend', 
+    images
+      .map(({ url, alt }) => `<li class="gallery-item"><img src="${url}" alt="${alt}"></li>`)
+      .join('')
+  );
+
+  gallery.style.display = 'flex';
+  gallery.style.gap = '10px';
+  gallery.style.listStyle = 'none';
+  gallery.style.padding = '0';
+  gallery.style.justifyContent = 'center';
+
+  document.querySelectorAll('.gallery-item img').forEach(img => {
+    img.style.width = '300px';
+    img.style.height = 'auto';
+    img.style.borderRadius = '8px';
+    img.style.boxShadow = '2px 2px 10px rgba(0, 0, 0, 0.2)';
+  });  
+// 4
+let counterValue = 0;
+
+const value = document.querySelector('#value');
+const decrement = document.querySelector('[data-action="decrement"]');
+const increment = document.querySelector('[data-action="increment"]');
+
+const increase = () => {
+  counterValue += 1;
+  value.textContent = counterValue;
+};
+
+const decrease = () => {
+  counterValue -= 1;
+  value.textContent = counterValue;
+};
+
+decrement.addEventListener('click', decrease);
+increment.addEventListener('click', increase);
